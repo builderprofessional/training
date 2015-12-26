b0b4295b473b4c5cdbe4bbdfd3425aef05b25f4a
Robert Flach <robert@point-of-rental.com>
2015-12-26 17:44:31 -0600

    adding engine to app kernel. moving assets for engine to higher level directory

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new ThirdEngine\PropelSOABundle\PropelSOABundle(),
             new Training\TrainingBundle\TrainingTrainingBundle(),
+						new Engine\EngineBundle\EngineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
