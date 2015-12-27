199958ab39dfeccd798c87438ab5983fa710a83a
Tony Vance <tony.vance@latentcodex.com>
2015-12-27 02:26:27 +0000

    getting top-level routing

--- src/Training/TrainingBundle/Controller/DefaultController.php
+++ src/Training/TrainingBundle/Controller/DefaultController.php
@@ -1,27 +1,21 @@
 <?php
 namespace Training\TrainingBundle\Controller;
 
-use ThirdEngine\PropelSOABundle\Http\PropelSOASuccessResponse;
-use ThirdEngine\PropelSOABundle\Controller\ServiceController;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 
-/**
- * @route /training/default
- */
-class DefaultController extends ServiceController
+
+class DefaultController extends Controller
 {
-  /**
-   * @Route("/default")
-   * @Method({"GET"})
-   *
-   * @param Request $request
-   */
-  public function getAction(Request $request)
+  public function indexAction()
   {
-    return new PropelSOASuccessResponse(['yo' => 1]);
+    return $this->render('TrainingTrainingBundle:Default:index.html.twig');
+  }
+
+  public function signupAction()
+  {
+    return $this->render('TrainingTrainingBundle:Default:signup.html.twig');
   }
 }
