d43e45168e5a8d9b9223cf51c8aae639d65bc2c2
Tony Vance <tony.vance@latentcodex.com>
2016-01-03 22:53:14 +0000

    adding recurring plan and first class to the product list

---
+++ app/phinx/migrations/20160103224456_add_plan_and_quick_books_for_homebuilders_product.php
@@ -0,0 +1,36 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddPlanAndQuickBooksForHomebuildersProduct extends AbstractMigration
+{
+  /**
+   * Change Method.
+   *
+   * Write your reversible migrations using this method.
+   *
+   * More information on writing migrations is available here:
+   * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+   *
+   * The following commands can be used in this method and Phinx will
+   * automatically reverse them when rolling back:
+   *
+   *    createTable
+   *    renameTable
+   *    addColumn
+   *    renameColumn
+   *    addIndex
+   *    addForeignKey
+   *
+   * Remember to call "create()" or "update()" and NOT "save()" when working
+   * with the Table class.
+   */
+  public function change()
+  {
+    $this->execute("
+      INSERT INTO billing_product (date_created, code, name, amount, description, position, one_time_flag)
+        VALUES (NOW(), 'QUICKBOOKS_FOR_HOMEBUILDERS', 'QuickBooks for Homebuilders', 2990000, '', 1, 1),
+          (NOW(), 'NEVER_STOP_LEARNING', 'Never Stop Learning', 290000, '', 0, 0);
+    ");
+  }
+}
