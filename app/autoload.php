4590744e3f9edc0b9bad7ffc66f70abf7072f0e7
Tony Vance <tony.vance@latentcodex.com>
2016-01-03 22:38:19 +0000

    adding latest engine and fixed ut system

--- app/autoload.php
+++ app/autoload.php
@@ -5,10 +5,8 @@ use ThirdEngine\PropelSOABundle\Utility\DocBlockUtility;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
-/**
- * @var ClassLoader $loader
- */
 $loader = require __DIR__.'/../vendor/autoload.php';
+require_once(__DIR__ . '/../src/Engine/EngineBundle/Validation/is_email.php');
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
