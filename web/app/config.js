46463e6aedb18cc55d889bbf5efc46fe4426ae7c
Robert Flach <robert@point-of-rental.com>
2016-01-04 10:35:31 -0600

    Updating everything

---
+++ web/app/config.js
@@ -0,0 +1,8 @@
+/**
+ * This file contains constants that can be used in the javascript piece of the app that come from
+ * configuration/data from the server side at build time.
+ *
+ * @copyright 2014 Latent Codex / Third Engine Software
+ */
+
+var braintreeCSE = "MIIBCgKCAQEAqdbS2mXVEU1Du/NltnNbrzfxI4yVo0EJ33Y/yl/UYxfGC4jWDjg2c6HjsUVlpEYpYWVK+u3RXZhTGMltvGYRsYIhjc74wKJZTz69ckjffKfmoFBnJ+l8ViUwFNY53byLWqMEY93KbuP8KiTeXqGvisS9koSbO2SUwUEsHwMa4WpID2XNOE//56mPqmkbLGLJpLtHTaOiTdjgUGAaZpWyUeLOTok2VRpDUeKCVyGE3o9iyJgjnvZ0I+j09pSZ1Lk3GGYPTSf/TVjncyL0S930hd4Nv2LdeUW9yd9lVfObZ+GBFE0ZJZJMVfbZWr0BRusihRQsL4GTBTsimeT6KlBPwwIDAQAB";
