149aca3199304e24bf480c67247ba725952506ee
Tony Vance <tony.vance@latentcodex.com>
2016-01-04 20:05:54 +0000

    adding linked data to check course authorization

--- src/Training/TrainingBundle/Model/Course.php
+++ src/Training/TrainingBundle/Model/Course.php
@@ -1,9 +1,48 @@
 <?php
-
+/**
+ * This represents one training course.
+ *
+ * @copyright 2016 Third Engine Software
+ */
 namespace Training\TrainingBundle\Model;
+
 
 use Training\TrainingBundle\Model\om\BaseCourse;
 
+use Engine\BillingBundle\Model\ClientPeer;
+use Engine\BillingBundle\Model\ClientQuery;
+
+use ThirdEngine\Factory\Factory;
+
+
 class Course extends BaseCourse
 {
+  /**
+   * This method will determine if the current client or user has access to view our materials.
+   *
+   * @return int
+   */
+  public function isAuthorized()
+  {
+    $clientPeer = Factory::createNewObject(ClientPeer::class);
+    $clientId = $clientPeer->getWorkingClientId();
+
+    $client = Factory::createNewQueryObject(ClientQuery::class)->findOneByClientId($clientId);
+    foreach ($client->getClientProducts() as $clientProduct)
+    {
+      if ($clientProduct->getProductId() == $this->getBillingProductId())
+      {
+        // Our related product is in the list of products the client has access to, so they should be able to view this.
+        return 1;
+      }
+
+      if ($clientProduct->getProduct()->getCode() == 'NEVER_STOP_LEARNING')
+      {
+        // This client is on the all-access plan, and should always have access
+        return 1;
+      }
+    }
+
+    return 0;
+  }
 }
--- src/Training/TrainingBundle/Model/CoursePeer.php
+++ src/Training/TrainingBundle/Model/CoursePeer.php
@@ -1,9 +1,24 @@
 <?php
-
+/**
+ * This class represents one training course.
+ *
+ * @copyright 2016 Third Engine Software
+ */
 namespace Training\TrainingBundle\Model;
+
 
 use Training\TrainingBundle\Model\om\BaseCoursePeer;
 
+
+
 class CoursePeer extends BaseCoursePeer
 {
+  /**
+   * This is a definition of extra available data that can be retried from PropelSOA along with a course.
+   *
+   * @var array
+   */
+  public $linkedData = [
+    'Authorized' => 'isAuthorized',
+  ];
 }
