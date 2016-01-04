32a5213a1dd2f775909810d027c7124f207b1210
Tony Vance <tony.vance@latentcodex.com>
2016-01-04 22:28:02 +0000

    adding an answer controller

---
+++ src/Training/TrainingBundle/Controller/AnswerController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This controller exposes answers to questions to the training app.
+ *
+ * @copyright 2016 Third Engine Software
+ */
+namespace Training\TrainingBundle\Controller;
+
+
+use Training\TrainingBundle\Model\Answer;
+
+use Engine\EngineBundle\Controller\ModelBasedController;
+
+use ThirdEngine\Factory\Factory;
+use ThirdEngine\PropelSOABundle\Http\PropelSOAErrorResponse;
+use ThirdEngine\PropelSOABundle\Http\PropelSOASuccessResponse;
+use ThirdEngine\PropelSOABundle\Base\SymfonyClassInfo;
+use ThirdEngine\PropelSOABundle\Interfaces\Collectionable;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+
+/**
+ * @route /training/answer
+ */
+class AnswerController extends ModelBasedController implements Collectionable
+{
+  /**
+   * This method points our controller to the proper table.
+   */
+  public function setupClassInfo()
+  {
+    $this->classInfo = new SymfonyClassInfo();
+
+    $this->classInfo->namespace = 'Training';
+    $this->classInfo->bundle    = 'Training';
+    $this->classInfo->entity    = 'Answer';
+  }
+
+  /**
+   * This action will return a list of answers.
+   *
+   * @Route("/answer")
+   * @Method({"GET"})
+   *
+   * @param Request $request
+   */
+  public function getAction(Request $request)
+  {
+    return parent::getAction($request);
+  }
+}
