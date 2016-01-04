074c78f9c02a695f669c07b08338da759c166df2
Tony Vance <tony.vance@latentcodex.com>
2016-01-04 16:47:20 +0000

    start definition for mongo storage

--- src/Training/TrainingBundle/Model/QuestionPeer.php
+++ src/Training/TrainingBundle/Model/QuestionPeer.php
@@ -1,9 +1,25 @@
 <?php
-
+/**
+ * This class defines a question asked about a training course.
+ */
 namespace Training\TrainingBundle\Model;
+
 
 use Training\TrainingBundle\Model\om\BaseQuestionPeer;
 
+use Engine\EngineBundle\Model\DocumentProperty;
+
+use ThirdEngine\Factory\Factory;
+
+
 class QuestionPeer extends BaseQuestionPeer
 {
+  /**
+   * This constructor will set us up to store our question text in MongoDB.
+   */
+  public function __construct()
+  {
+    $questionDocumentProperty = Factory::createNewObject(DocumentProperty::class, ['question', 'getQuestion', 'setQuestion', $this]);
+    $this->documentProperties = [$questionDocumentProperty];
+  }
 }
