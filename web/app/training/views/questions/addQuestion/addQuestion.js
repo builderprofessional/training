977aa30513e0dd1ea56073bc23448e853ad1de01
Robert Flach <robert@point-of-rental.com>
2016-01-16 21:45:16 -0600

    5-levels

--- web/app/training/views/questions/addQuestion/addQuestion.js
+++ web/app/training/views/questions/addQuestion/addQuestion.js
@@ -1,8 +1,8 @@
 (function ()
 {
   trainingApp.directive("trainingViewAddQuestion", ['$http', 'PropelSOAService',
-                                                     'engAlert','$timeout', addQuestion]);
-  function addQuestion($http, PropelSOAService, engAlert,$timeout)
+                                                     'engAlert','$timeout','engValidation', addQuestion]);
+  function addQuestion($http, PropelSOAService, engAlert,$timeout,engValidation)
   {
     return {
       restrict: "A",
@@ -17,21 +17,30 @@
 
           $scope.Question.model.CourseId=$scope.course.getPk();
           $scope.Question.model.QuestionText='';
+          $scope.validator = engValidation.getValidator({'QuestionText':['required']});
+          $scope.validator.setRules({'QuestionText':['required']});
+          $scope.validator.watch($scope, $scope.Question.model);
           $scope.save = function ()
           {
             $scope.waiting=true;
             $timeout(function(){
               $scope.waiting=false;
             },3000);
-            $http.post(env_url+'/training/question',$scope.Question.model).then(function(result){
-              engAlert.success("Your Question has been submitted.");
-              $scope.Question = PropelSOAService.getNewObject('Training', 'Training', 'Question');
-              $scope.Question.model.CourseId=$scope.course.getPk();
-              $scope.Question.model.QuestionText='';
-              $scope.$parent.reloadData();
-            },
-            function(result){
-              engAlert.error("There was a problem submitting your question.");
+            $scope.validator.isValid().then(function(){
+              $http.post(env_url+'/training/question',$scope.Question.model).then(function(result){
+                engAlert.success("Your Question has been submitted.");
+                $scope.Question = PropelSOAService.getNewObject('Training', 'Training', 'Question');
+                $scope.Question.model.CourseId=$scope.course.getPk();
+                $scope.Question.model.QuestionText='';
+                $scope.$parent.reloadData();
+              },
+              function(result){
+                engAlert.error("There was a problem submitting your question.");
+                $scope.waiting=false;
+              });
+            },function(){
+              engAlert.error("You must submit a question to receive an answer.");
+              $scope.waiting=false;
             });
           };
         }
