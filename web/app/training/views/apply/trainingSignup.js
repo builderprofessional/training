1fe083b2c3010363007051b25d476a2e866c17e8
Robert Flach <robert@point-of-rental.com>
2016-03-05 13:11:57 -0600

    loader for initial signup

--- web/app/training/views/apply/trainingSignup.js
+++ web/app/training/views/apply/trainingSignup.js
@@ -17,13 +17,16 @@
             $scope.validator.watch($scope, $scope.SignUp);
           });
           $scope.save = function () {
+            $scope.loading=true;
             $scope.validator.isValid($scope.SignUp).then(function(result) {
               $http.post(env_url + "/public/training/signup/json", $scope.SignUp).then(function (result) {
                 engAlert.success("Your request has been received, please check your email for the next steps.");
                 $scope.SignUp.signupId = result.data.Data.signupId;
+                $scope.loading=false;
               });
             },function(result){
               engAlert.error("We didn't quite get that.  Check the signup form for errors and try again.");
+              $scope.loading=false;
               return result;
             });
           };
